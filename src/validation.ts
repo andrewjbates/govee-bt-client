661f99f2743ad119c3e7d69e7f7d54793bcad516
Andrew Bates <andrew@hallmark.dev>
2024-01-17 01:48:36 -0600

    fix(validation): use startsWith instead of includes

--- src/validation.ts
+++ src/validation.ts
@@ -6,12 +6,12 @@ const h5101_uuid_rev = "0100";
 const h5179_uuid_rev = "0188";
 
 export const isHt5074 = (hex: string) =>
-    hex.includes(h5074_uuid_rev) && hex.length == 18; // Govee H5074
+    hex.startsWith(h5074_uuid_rev) && hex.length == 18; // Govee H5074
 export const isHt5075 = (hex: string) =>
-    hex.includes(h5075_uuid_rev) && hex.length == 16; // Govee H5072/H5075
-export const isHt5101 = (hex: string) => hex.includes(h5101_uuid_rev); // Govee H5100/H5101/H5102
+    hex.startsWith(h5075_uuid_rev) && hex.length == 16; // Govee H5072/H5075
+export const isHt5101 = (hex: string) => hex.startsWith(h5101_uuid_rev); // Govee H5100/H5101/H5102
 export const isHt5179 = (hex: string) =>
-    hex.includes(h5179_uuid_rev) && hex.length == 22; // Govee H5179
+    hex.startsWith(h5179_uuid_rev) && hex.length == 22; // Govee H5179
 
 export const isValidPeripheral = (peripheral: noble.Peripheral) => {
     const { address, advertisement } = peripheral;
